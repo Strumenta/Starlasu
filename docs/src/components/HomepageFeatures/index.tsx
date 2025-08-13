2c7f760810ca30a1bb0672127a82c739c845c61a
Federico Tomassetti <federico@strumenta.com>
2025-08-13 08:37:57 +0200

    Working on CI for building and deploying documentation

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import { JSX } from "react/jsx-runtime";
 
 type FeatureItem = {
   title: string;
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -6,6 +6,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+import { JSX } from "react/jsx-runtime";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
